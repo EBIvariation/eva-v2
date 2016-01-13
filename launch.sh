c3f30e26680245b61ebfe32e2008dbcb84f9f477
jose miguel mut <jomutlo@gmail.com>
2016-01-13 16:54:02 +0000

    pipeline: updating launch script

--- launch.sh
+++ launch.sh
@@ -1,4 +1,5 @@
 java -jar eva-pipeline/target/eva-pipeline-0.1.jar \
+ --spring.batch.job.names=variantJob \
  input=data/small.vcf \
  fileId=5 \
  aggregated=NONE \
@@ -8,10 +9,15 @@ java -jar eva-pipeline/target/eva-pipeline-0.1.jar \
  outputDir= \
  pedigree= \
  dbName=batch \
- samples=sample1,sample2,sample3,sample4 \
  storageEngine=mongodb \
  compressGenotypes=true \
  compressExtension=.gz \
  includeSrc=FIRST_8_COLUMNS \
- skipLoad=false
+ vepInput=out/variants.preannot.tsv.gz \
+ vepPath="/nfs/production2/eva/VEP/ensembl-tools-release-78/scripts/variant_effect_predictor/variant_effect_predictor.pl" \
+ vepParameters="--force_overwrite --cache --cache_version 78 -dir /nfs/production2/eva/VEP/cache_1 --offline -o STDOUT --species homo_sapiens --everything" \
+ vepFasta="--fasta /nfs/production2/eva/VEP/cache_1/homo_sapiens/78_GRCh37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa" \
+ vepOutput=out/variants.annot.tsv.gz \
+ skipAnnotCreate=true \
+ opencga.app.home=/opt/opencga
 
